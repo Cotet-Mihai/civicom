065f53fb6eaa26771a2e5914770615d76c053fdc
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2026-02-04 09:01:00 +0200

    test(vercel): Reimplemented the map in the gatheringMap.tsx component

--- features/protest/components/steps/locationSteps/gatheringMap.tsx
+++ features/protest/components/steps/locationSteps/gatheringMap.tsx
@@ -1,45 +1,44 @@
-import React, {JSX} from "react";
-// import type L from "leaflet";
-//
-// import {defaultLocation} from "@/features/protest/protest.config";
-//
-// import {
-//     Map,
-//     MapDrawControl,
-//     MapDrawDelete,
-//     MapDrawEdit, MapDrawMarker,
-//     MapLocateControl,
-//     MapTileLayer, useLeaflet
-// } from "@/components/ui/map";
-//
-// import MapSearchControlWrapper from "@/utils/MapSearch";
-// import {extractMarkers, removeDuplicateMarkers} from "@/utils/mapHelper";
+import React, {JSX, useRef} from "react";
+
+import {defaultLocation} from "@/features/protest/protest.config";
+
+import {
+    Map,
+    MapDrawControl,
+    MapDrawDelete,
+    MapDrawEdit, MapDrawMarker,
+    MapLocateControl,
+    MapTileLayer
+} from "@/components/ui/map";
+
+import MapSearchControlWrapper from "@/utils/MapSearch";
+import {extractMarkers, removeDuplicateMarkers} from "@/utils/mapHelper";
 
 import {StandardStepProp} from "@/features/protest/types/type";
 import {Gathering} from "@/features/protest/types/locationTypes";
-// import {MarkerShape} from "@/types/map";
+import {MarkerShape} from "@/types/map";
 
 
 export default function GatheringMap({dataState}: StandardStepProp<Gathering>): JSX.Element {
     const {set: onChange} = dataState;
 
-    // const mapRef = useRef<L.Map | null>(null)
+    const mapRef = useRef<L.Map | null>(null);
     // const {L} = useLeaflet();
-    //
-    // function handleOnChange(layers: L.FeatureGroup) {
-    //     removeDuplicateMarkers(layers);
-    //
-    //     const marker: MarkerShape = extractMarkers(layers)[0];
-    //
-    //     onChange((prev: Gathering) => ({
-    //             ...prev,
-    //             location: {
-    //                 lat: marker.position.lat,
-    //                 lng: marker.position.lng
-    //             },
-    //         })
-    //     );
-    // }
+
+    function handleOnChange(layers: L.FeatureGroup) {
+        removeDuplicateMarkers(layers);
+
+        const marker: MarkerShape = extractMarkers(layers)[0];
+
+        onChange((prev: Gathering) => ({
+                ...prev,
+                location: {
+                    lat: marker.position.lat,
+                    lng: marker.position.lng
+                },
+            })
+        );
+    }
 
     return (
         // <Map
@@ -58,6 +57,23 @@ export default function GatheringMap({dataState}: StandardStepProp<Gathering>): 
         //         <MapDrawDelete />
         //     </MapDrawControl>
         // </Map>
-        <h1>Gatthering</h1>
+
+        <Map
+            center={defaultLocation}
+            zoom={13}
+            ref={mapRef}
+            className="h-[500px]"
+        >
+            <MapTileLayer />
+            <MapSearchControlWrapper/>
+            <MapLocateControl />
+            <MapDrawControl
+                onLayersChange={(layers: L.FeatureGroup) => handleOnChange(layers)}
+            >
+                <MapDrawMarker />
+                <MapDrawEdit />
+                <MapDrawDelete />
+            </MapDrawControl>
+        </Map>
     );
 }
