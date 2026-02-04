962a12b9a3f5bd987de5fe4e10811c9a368c30ab
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2026-02-04 08:41:53 +0200

    fix(vercel): Changed the gatheringMap.tsx component to return only a string back to test if the bug is alpha in the map. Plus deleted the rest of the maps with associated states

--- features/protest/components/steps/locationSteps/gatheringMap.tsx
+++ features/protest/components/steps/locationSteps/gatheringMap.tsx
@@ -1,62 +1,63 @@
-import React, {JSX, useRef} from "react";
-import L from "leaflet";
-
-import {defaultLocation} from "@/features/protest/protest.config";
-
-import {
-    Map,
-    MapDrawControl,
-    MapDrawDelete,
-    MapDrawEdit, MapDrawMarker,
-    MapLocateControl,
-    MapTileLayer, useLeaflet
-} from "@/components/ui/map";
-
-import MapSearchControlWrapper from "@/utils/MapSearch";
-import {extractMarkers, removeDuplicateMarkers} from "@/utils/mapHelper";
+import React, {JSX} from "react";
+// import type L from "leaflet";
+//
+// import {defaultLocation} from "@/features/protest/protest.config";
+//
+// import {
+//     Map,
+//     MapDrawControl,
+//     MapDrawDelete,
+//     MapDrawEdit, MapDrawMarker,
+//     MapLocateControl,
+//     MapTileLayer, useLeaflet
+// } from "@/components/ui/map";
+//
+// import MapSearchControlWrapper from "@/utils/MapSearch";
+// import {extractMarkers, removeDuplicateMarkers} from "@/utils/mapHelper";
 
 import {StandardStepProp} from "@/features/protest/types/type";
 import {Gathering} from "@/features/protest/types/locationTypes";
-import {MarkerShape} from "@/types/map";
+// import {MarkerShape} from "@/types/map";
 
 
 export default function GatheringMap({dataState}: StandardStepProp<Gathering>): JSX.Element {
     const {set: onChange} = dataState;
 
-    const mapRef = useRef<L.Map | null>(null)
-    const {L} = useLeaflet();
-
-    function handleOnChange(layers: L.FeatureGroup) {
-        removeDuplicateMarkers(layers);
-
-        const marker: MarkerShape = extractMarkers(layers)[0];
-
-        onChange((prev: Gathering) => ({
-                ...prev,
-                location: {
-                    lat: marker.position.lat,
-                    lng: marker.position.lng
-                },
-            })
-        );
-    }
+    // const mapRef = useRef<L.Map | null>(null)
+    // const {L} = useLeaflet();
+    //
+    // function handleOnChange(layers: L.FeatureGroup) {
+    //     removeDuplicateMarkers(layers);
+    //
+    //     const marker: MarkerShape = extractMarkers(layers)[0];
+    //
+    //     onChange((prev: Gathering) => ({
+    //             ...prev,
+    //             location: {
+    //                 lat: marker.position.lat,
+    //                 lng: marker.position.lng
+    //             },
+    //         })
+    //     );
+    // }
 
     return (
-        <Map
-            center={defaultLocation}
-            zoom={13}
-            ref={mapRef}
-        >
-            <MapTileLayer />
-            <MapSearchControlWrapper/>
-            <MapLocateControl />
-            <MapDrawControl
-                onLayersChange={(layers: L.FeatureGroup) => handleOnChange(layers)}
-            >
-                <MapDrawMarker />
-                <MapDrawEdit />
-                <MapDrawDelete />
-            </MapDrawControl>
-        </Map>
+        // <Map
+        //     center={defaultLocation}
+        //     zoom={13}
+        //     ref={mapRef}
+        // >
+        //     <MapTileLayer />
+        //     <MapSearchControlWrapper/>
+        //     <MapLocateControl />
+        //     <MapDrawControl
+        //         onLayersChange={(layers: L.FeatureGroup) => handleOnChange(layers)}
+        //     >
+        //         <MapDrawMarker />
+        //         <MapDrawEdit />
+        //         <MapDrawDelete />
+        //     </MapDrawControl>
+        // </Map>
+        <h1>Gatthering</h1>
     );
 }
