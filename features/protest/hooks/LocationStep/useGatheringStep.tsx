755d81ad68ef26bd959c19354566217e9c857f86
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2026-02-05 19:24:33 +0200

    test(vercel): Changed the import in useGatheringStep to directly import the component

--- features/protest/hooks/LocationStep/useGatheringStep.tsx
+++ features/protest/hooks/LocationStep/useGatheringStep.tsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-import GatheringMap from "@/features/protest/components/steps/locationSteps/gatheringMap.client";
+import GatheringMap from "@/features/protest/components/steps/locationSteps/gatheringMap";
 
 import {Gathering} from "@/features/protest/types/locationTypes";
 import {checkLocation} from "@/features/protest/utils";
