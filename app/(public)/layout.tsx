cbea4207e22447f5c3af714fedf4c3dd4a9f9596
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2026-02-07 16:55:03 +0200

    chore(vercel): add Vercel Speed Insights and Analytics

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PublicLayout({children,} : Readonly<{ children: React.ReactNode }>) {
+    return(
+        <>
+            <main className={"min-h-screen"}>
+                {children}
+            </main>
+        </>
+    )
+}
---
+++ app/(public)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            <div className={'md:h-screen bg-white flex flex-col pt-12'}>
+                <h1>Test</h1>
+            </div>
+        </div>
+    );
+}
