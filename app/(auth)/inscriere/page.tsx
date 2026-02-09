9ff1aca2dde24db8326871103de0621d68c159c9
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2026-02-10 00:21:20 +0200

    fix(page): update Civicom link href to /inscriere in page.tsx

--- app/(auth)/inscriere/page.tsx
+++ app/(auth)/inscriere/page.tsx
@@ -8,7 +8,7 @@ export default function SignupPage() {
         <div className="grid min-h-svh lg:grid-cols-2">
             <div className="flex flex-col p-6 md:p-10">
                 <div className="flex justify-center md:justify-start">
-                    <Link href="/public" className="flex items-center font-extrabold text-primary">
+                    <Link href="/" className="flex items-center font-extrabold text-primary">
                         CIVICOM✨
                     </Link>
                 </div>
